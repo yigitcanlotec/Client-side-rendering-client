dbfae62bedc75418b504d0c51e304910da8494fc
YigitJohn <yigitcanustek@gmail.com>
2023-10-16 23:43:54 +0300

    Added task add, filter, delete and mark functionality.

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ function sendLoginRequest(e) {
     };
 
     
-    const headers = {
-        Authorization: `Bearer ${token}`,
-    };
+   
     
     axios
         .post('http://localhost:3000/', postData, {
@@ -31,7 +29,9 @@ function sendLoginRequest(e) {
         })
         .then((response) => {
             token = response.data.token; // Extract token from the response
-
+            const headers = {
+                Authorization: `Bearer ${token}`,
+            };
             console.log(response.data); // This will log the response from '/redirect'
         })
         .then((response) => {
@@ -161,7 +161,7 @@ function loginPage() {
     domCreateElement('button', {
         type: 'submit',
         innerText: 'Sign In',
-        onclick: sendLoginRequest,
+        onclick: homePage, //sendLoginRequest,
     }).appendToLast('input-container');
 
     domCreateElement('script', {
@@ -241,7 +241,81 @@ function registerPage() {
     }).appendToLast('body');
 }
 
-function homePage() {
+
+function deleteAndCreateTableToQueryResult(query) {
+    
+    const queryTable = document.getElementById('table');
+    queryTable.removeChild(document.getElementById('table-body'));
+    domCreateElement('tbody', { id: 'table-body', className: 'table-body' }).appendToLast(
+        'table'
+    );
+
+    const tableBody = document.querySelector('#table tbody');
+   
+        query.data.forEach(item => {
+        const row = tableBody.insertRow(); // Insert a new row to the table body
+    
+        // Add cells (columns) to the row
+        const cell1 = row.insertCell(); // Create a new cell for column 1
+        const cell2 = row.insertCell();
+        const cell3 = row.insertCell();
+        const cell4 = row.insertCell();
+        const cell5 = row.insertCell();
+      // Create a new cell for column 2
+    
+        // Populate cell data with the corresponding values from the data object
+        cell1.textContent = item.id;
+        cell2.textContent = item.title;
+        cell3.textContent = item.assignee;
+        cell4.textContent = item.done;
+        const button = document.createElement('input');
+        button.type = 'button'; 
+        button.value = 'Sil';
+        button.id = 'sil';
+        cell5.appendChild( button);
+
+        const button2 = document.createElement('input');
+        button2.type = 'button'; 
+        button2.value = 'Edit';
+        button2.id = 'edit';
+        cell5.appendChild(button2); 
+
+        const button3 = document.createElement('input');
+        button3.type = 'button'; 
+        button3.value = `Mark as ${!item.done}`;
+        button3.id = 'mark';
+        cell5.appendChild(button3); 
+     
+      });
+}
+
+
+async function filter () {
+    const query = await axios.get(`http://localhost:3000/api/v1/tasks?filter=${document.getElementById('select-done').value}`);
+  
+    deleteAndCreateTableToQueryResult(query);
+}
+
+
+async function addTask(){
+    const postData = {
+        assignee: 'value',
+        title: document.getElementById('title-text').value,
+        done: document.getElementById('done-check').checked
+      };
+     console.log(document.getElementById('done-check').checked);
+    
+    axios.post('http://localhost:3000/api/v1/task/insert', postData).then((query) => {
+        filter();
+    });
+    
+}
+
+
+async function homePage() {
+
+    const tableData = await axios.get('http://localhost:3000/api/v1/tasks/');
+    // console.log(tableData);
     // Get a reference to the body element
     const oldBodyElement = document.body;
 
@@ -256,7 +330,7 @@ function homePage() {
 
     domCreateElement('link', {
         rel: 'stylesheet',
-        href: '/css/user.css',
+        href: '/css/home.css',
     }).appendToLast('body');
 
     domCreateElement('div', { className: 'first-container' }).appendToLast(
@@ -296,4 +370,28 @@ function homePage() {
     domCreateElement('th', { innerText: 'done' }).appendToLast(
         'tr'
         ); 
+    domCreateElement('tbody', { id: 'table-body', className: 'table-body' }).appendToLast(
+        'table'
+    );
+   
+    deleteAndCreateTableToQueryResult(tableData);
+
+    domCreateElement('div', {className: 'input-container'}).appendToLast('first-container');
+    domCreateElement('div', {className: 'inputForm', id: 'inputForm'}).appendToLast('input-container');
+    domCreateElement('input', {type: 'checkbox', id: 'done-check', name:'done'}).appendToLast('inputForm');
+    domCreateElement('input', {type: 'text', id: 'title-text', name:'title'}).appendToLast('inputForm');
+    domCreateElement('input', {type: 'submit', id: 'submit', value:'Submit', onclick: addTask}).appendToLast('inputForm');
+    
+
+    domCreateElement('div', {className: 'second-container'}).appendToLast('body');
+    domCreateElement('div', {className: 'filter'}).appendToLast('second-container');
+    domCreateElement('div', {className: 'form'}).appendToLast('filter');
+    domCreateElement('select', {className: 'select-done', id: 'select-done'}).appendToLast('form');
+    domCreateElement('option', {value: 'all', selected: true, textContent: 'All'}).appendToLast('select-done');
+    domCreateElement('option', {value: 'true', textContent: 'true'}).appendToLast('select-done');
+    domCreateElement('option', {value: 'false', textContent: 'false'}).appendToLast('select-done');
+    domCreateElement('input', {type: 'submit', name: 'filter', id: 'filter', value: 'filter', onclick: filter}).appendToLast('form');
+    
+ 
+    
 }
